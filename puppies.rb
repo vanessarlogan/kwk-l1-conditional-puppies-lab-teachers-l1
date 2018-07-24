e5a1ce4cf02d86f9deb0e58f5d8de34582262339
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-07-24 17:52:18 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -5,6 +5,7 @@ def no_puppies(puppies)
     puts "No more puppies!"
   else
     puts "Well done Rachel!"
+  end
 end
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
